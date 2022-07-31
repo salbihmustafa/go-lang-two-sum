d71ca8ade42067849c9645461311fc6921c8b70e
Salbih Mustafa <salbihdmustafa@gmail.com>
2022-07-31 17:58:03 -0500

    initial commit

---
+++ two-sum/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	myTarget := 9
+	myNums := []int{2, 7, 11, 15}
+	fmt.Println(myNums, myTarget)
+}
+
+// func twoSums(nums []int, target int) []int {
+// 	fmt.Println(nums)
+// }
